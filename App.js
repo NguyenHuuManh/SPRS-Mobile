2b170ce04ae57bca32e33e5b0e62412d379a07dd
NguyenHuuManh <se.huumanh@gmail.com>
2021-08-21 22:21:35 +0700

    no message

--- App.js
+++ App.js
@@ -10,103 +10,130 @@ import React, { useState, useEffect } from 'react';
 import type { Node } from 'react';
 import GetLocation from 'react-native-get-location'
 import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
   useColorScheme,
   View,
+  Text,
+  TouchableOpacity,
+  Button
 } from 'react-native';
-
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
 import MapView, { Marker, PROVIDER_GOOGLE } from 'react-native-maps';
-
-const Section = ({ children, title }): Node => {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-};
-
+import { firebaseApp } from './Firebase/config';
+import auth from '@react-native-firebase/auth';
 const App: () => Node = () => {
-  const isDarkMode = useColorScheme() === 'dark';
-  const [region, setRegion] = useState({
-    latitude: 21.0263084,
-    longitude: 105.7709134,
-    latitudeDelta: 0.0922,
-    longitudeDelta: 0.0421,
-  })
 
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
+  // const [region, setRegion] = useState({
+  //   latitude: 21.0263084,
+  //   longitude: 105.7709134,
+  //   latitudeDelta: 0.0922,
+  //   longitudeDelta: 0.0421,
+  // });
+  // const [marker, setMarker] = useState({
+  //   latitude: 21.0263084,
+  //   longitude: 105.7709134
+  // })
 
-  const getCurrentLocation = () => {
-    GetLocation.getCurrentPosition({
-      enableHighAccuracy: true,
-      timeout: 15000,
-    })
-      .then(location => {
-        console.log(location, "location");
+
+  // const getCurrentLocation = () => {
+  //   GetLocation.getCurrentPosition({
+  //     enableHighAccuracy: true,
+  //     timeout: 15000,
+  //   })
+  //     .then(location => {
+  //       console.log(location, "location");
+  //     })
+  //     .catch(error => {
+  //       const { code, message } = error;
+  //       console.warn(code, message);
+  //     })
+  // }
+  // useEffect(() => {
+  //   getCurrentLocation();
+  // }, [])
+
+  // const onChangeRegion = (region) => {
+  //   setRegion(region)
+  // }
+  // return (
+  //   <View style={{ position: 'relative', height: 600 }}>
+  //     <MapView
+  //       provider={PROVIDER_GOOGLE}
+  //       style={{ left: 0, right: 0, top: 0, bottom: 0, position: 'absolute' }}
+  //       showsUserLocation
+  //       region={region}
+  //       // onRegionChangeComplete={onChangeRegion}
+
+  //       onPress={(e) => {
+  //         setMarker(e.nativeEvent.coordinate)
+  //       }}
+  //     >
+  //       <Marker
+  //         coordinate={marker}
+  //         title={"marker.title"}
+  //         description={"marker.description"}
+  //       />
+  //     </MapView>
+  //   </View>
+  // );
+  // Set an initializing state whilst Firebase connects
+  const [initializing, setInitializing] = useState(true);
+  const [user, setUser] = useState();
+
+  // Handle user state changes
+  function onAuthStateChanged(user) {
+    console.log("user", user)
+    setUser(user);
+    if (initializing) setInitializing(false);
+  }
+
+  useEffect(() => {
+    const subscriber = auth().onAuthStateChanged(onAuthStateChanged);
+    return subscriber; // unsubscribe on unmount
+  }, []);
+
+
+  const signin = () => {
+    console.log('signed in!');
+    auth()
+      .signInWithEmailAndPassword('manhnhhe130564@fpt.edu.vn', 'manhtkmd')
+      .then((e) => {
+        console.log('signed in!');
+        console.log('e', e);
+        setUser(e);
       })
       .catch(error => {
-        const { code, message } = error;
-        console.warn(code, message);
-      })
-  }
-  useEffect(() => {
-    getCurrentLocation();
-  }, [])
+        if (error.code === 'auth/email-already-in-use') {
+          console.log('That email address is already in use!');
+        }
 
-  const onChangeRegion = (region) => {
-    setRegion(region)
+        if (error.code === 'auth/invalid-email') {
+          console.log('That email address is invalid!');
+        }
+
+        console.error(error);
+      });
   }
+
+
+  if (initializing) return null;
+
+  // if (!user) {
+  //   return (
+  //     <>
+  //       <View style={{ width: 200, padding: 20 }}>
+  //         <TouchableOpacity onPress={() => { signin() }}>
+  //           <Text>Login</Text>
+  //         </TouchableOpacity>
+  //       </View>
+
+  //     </>
+  //   );
+  // }
+
   return (
-    <View style={{ position: 'relative', height: 800 }}>
-      <MapView
-        mapType={"standard"}
-        provider={PROVIDER_GOOGLE}
-        style={{ left: 0, right: 0, top: 0, bottom: 0, position: 'absolute' }}
-        initialRegion={{
-          latitude: 21.0263084,
-          longitude: 105.7709134,
-          latitudeDelta: 0.0922,
-          longitudeDelta: 0.0421,
-        }}
-        onPress={(e) => { console.log(e, "e") }}
-      >
-        <Marker
-          coordinate={{ latitude: 21.0263084, longitude: 105.7709134 }}
-          title={"marker.title"}
-          description={"marker.description"}
-        />
-      </MapView>
+    <View>
+      <View style={{ width: 200, padding: 20 }}>
+        <Button onPress={signin} title="Login" />
+      </View>
     </View>
   );
 };
